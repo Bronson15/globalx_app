0c9eaafc91bfc1c841b50a2e039e1cf5e78ca6d8
Bronson Lane <bronsonglane@me.com>
2018-07-30 10:51:10 -0400

    renames 'Users' to 'Participants'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'participants/new'
   root 'static_pages#home'
   get  '/blog',      to: 'static_pages#blog'
   get  '/trips',     to: 'static_pages#trips'
--- test/controllers/users_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get user_signup_path
-    assert_response :success
-  end
-
-end
